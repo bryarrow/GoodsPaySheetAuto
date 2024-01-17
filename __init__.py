84bc275e04ce36c39231c997d3eb16db9b8e6488
bryarrow <220811010104@qhu.edu.cn>
2024-01-17 16:24:54 +0800

    添加许可证信息 add LICENSE

--- __init__.py
+++ __init__.py
@@ -4,6 +4,16 @@
 全自动谷团肾表生成器
 
 by Berry(GitHub@bryarrow)
+
+Copyright (c) 2024 Yang Yunfei
+GoodsPaySheetAuto is licensed under Mulan PSL v2.
+You can use this software according to the terms and conditions of the Mulan PSL v2.
+You may obtain a copy of Mulan PSL v2 at:
+         https://license.coscl.org.cn/MulanPSL2
+THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+See the Mulan PSL v2 for more details.
 """
 
 import time
